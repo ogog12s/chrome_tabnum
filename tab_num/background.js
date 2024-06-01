5b2987f68ea930495e36566947f8f58db273bcce
shinichi-ogino <s_ogino2@ga-tech.co.jp>
2024-06-01 21:13:45 +0900

    フォーカスしていないタブも編集する

--- tab_num/background.js
+++ tab_num/background.js
@@ -1,3 +1,7 @@
+function updateTabTitle(title) {
+  document.title = title;
+}
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "getTabIndex") {
     chrome.tabs.query({ currentWindow: true }, (tabs) => {
@@ -5,5 +9,25 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       sendResponse({ index: index + 1 });
     });
     return true;
+  }
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && tab.active) {
+    chrome.tabs.query({ currentWindow: true }, (tabs) => {
+      for (let tab of tabs) {
+        if (tab.id !== tabId) {
+          const pureTitle = tab.title.replace(/^\d+:/, '');
+          const newTitle = `${tab.index + 1}: ${pureTitle}`;
+
+          chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: updateTabTitle,
+            args: [newTitle]
+          });
+        }
+      }
+    });
+    sendResponse({ status: "ok" });
   }
 });
